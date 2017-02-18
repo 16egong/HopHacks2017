8ac8ba092f8052b29b4743c0c0560f6a534c15ed
Acheev Bhagat <acheevbhagat@hotmail.com>
2017-02-18 17:23:55 -0500

    Added media player

--- HopHacks2017SADAlarm/app/src/main/java/com/hackathon/hophacks2017sadalarm/ScheduleService.java
+++ HopHacks2017SADAlarm/app/src/main/java/com/hackathon/hophacks2017sadalarm/ScheduleService.java
@@ -2,6 +2,7 @@ package com.hackathon.hophacks2017sadalarm;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.media.MediaPlayer;
 
 /**
  * Created by achee on 2/18/2017.
@@ -22,7 +23,8 @@ public class ScheduleService extends IntentService
     public void onHandleIntent(Intent intent)
     {
 
-        MainActivity.b = 1;
+        MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.foghorn);
+        mp.start();
         AlarmReceiver.completeWakefulIntent(intent);
 
     }
