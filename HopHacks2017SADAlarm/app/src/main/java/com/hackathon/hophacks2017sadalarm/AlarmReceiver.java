be255dd25951635b47fed55a129dcc984f64a5ab
themappel <michael_appel@outlook.com>
2017-02-18 18:43:02 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	HopHacks2017SADAlarm/app/src/main/java/com/hackathon/hophacks2017sadalarm/BootReceiver.java

--- HopHacks2017SADAlarm/app/src/main/java/com/hackathon/hophacks2017sadalarm/AlarmReceiver.java
+++ HopHacks2017SADAlarm/app/src/main/java/com/hackathon/hophacks2017sadalarm/AlarmReceiver.java
@@ -1,107 +1,39 @@
 package com.hackathon.hophacks2017sadalarm;
 
+import android.app.Activity;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.media.MediaPlayer;
-import android.support.v4.content.WakefulBroadcastReceiver;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.PowerManager;
+import android.util.Log;
+import android.widget.Toast;
 
-import java.util.Calendar;
 
-/**
- * Created by achee on 2/17/2017.
- */
+public class AlarmReceiver extends BroadcastReceiver {
 
-public class AlarmReceiver extends WakefulBroadcastReceiver
-{
-
-    private AlarmManager manager; //Needed for access to System Alarm Services
-    private PendingIntent alarmIntent; //Triggered when alarm fires
-
-    //Begin the alarm service when called
     @Override
-    public void onReceive(Context context, Intent intent)
-    {
+    public void onReceive(Context context, Intent intent) {
 
-        MediaPlayer mp = MediaPlayer.create(context, R.raw.foghorn);
-        mp.start();
+        String state = intent.getExtras().getString("extra");
+        Log.e("MyActivity", "In the receiver with " + state);
 
-    }
+        String richard_id = intent.getExtras().getString("quote id");
+        Log.e("Richard quote is" , richard_id);
 
-    //Sets and saves the alarm
-    public void setAlarm(Context context, int hour, int minute)
-    {
-        //Create services necessary to create the alarm
-        manager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-        Intent intent = new Intent(context, AlarmReceiver.class);
-        alarmIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
-        Calendar calendar = Calendar.getInstance();
-        /*
-        Convert data here from input using analog clock in UI
-        to calendar inputtable data, then use these calendar functions
-        to set a permanent alarm
+        Intent serviceIntent = new Intent(context,RingtonePlayingService.class);
+        serviceIntent.putExtra("extra", state);
+        serviceIntent.putExtra("quote id", richard_id);
 
-        ex.
-
-        calendar.set(Calendar.HOUR_OF_DAY, 8);
-        calendar.set(Calendar.MINUTE, 30);
-
-        Sets alarm for 8:30 am
-         */
-        calendar.set(Calendar.HOUR_OF_DAY, hour);
-        calendar.set(Calendar.MINUTE, minute);
-
-        //Set alarm to repeat at the same time everyday
-        /*
-        manager.setInexactRepeating(
-                AlarmManager.RTC_WAKEUP,
-                calendar.getTimeInMillis(),
-                AlarmManager.INTERVAL_DAY,
-                alarmIntent
-        );
-        */
-
-        //Enable receiver to automatically restart the alarm whenever the
-        //phone reboots
-        /*
-        ComponentName receiver = new ComponentName(context, BootReceiver.class);
-        PackageManager pm = context.getPackageManager();
-
-        pm.setComponentEnabledSetting(
-                receiver,
-                PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
-                PackageManager.DONT_KILL_APP
-        );
-        */
-    }
-
-    //Cancels the alarm
-    public void cancelAlarm(Context context)
-    {
-
-        //If alarm has been set, cancel it
-        if (manager != null)
-        {
-            manager.cancel(alarmIntent);
-        }
-
-        //Disable boot receiver so it doesn't automatically reset alarm
-        //when phone reboots
-        /*
-        ComponentName receiver = new ComponentName(context, BootReceiver.class);
-        PackageManager pm = context.getPackageManager();
-
-        pm.setComponentEnabledSetting(
-                receiver,
-                PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
-                PackageManager.DONT_KILL_APP
-        );
-        */
-
+        context.startService(serviceIntent);
     }
 
 }
