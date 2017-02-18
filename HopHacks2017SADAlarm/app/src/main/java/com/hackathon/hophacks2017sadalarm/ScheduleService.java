78fdf18bcc1eaafcb3af01cb203739aeda97ee48
Acheev Bhagat <acheevbhagat@hotmail.com>
2017-02-18 01:18:34 -0500

    Create ScheduleService

---
+++ HopHacks2017SADAlarm/app/src/main/java/com/hackathon/hophacks2017sadalarm/ScheduleService.java
@@ -0,0 +1,18 @@
+package com.hackathon.hophacks2017sadalarm;
+
+import android.app.IntentService;
+
+/**
+ * Created by achee on 2/18/2017.
+ */
+
+public class ScheduleService extends IntentService
+{
+
+    @Override
+    public void onHandleIntent(Intent intent)
+    {
+
+    }
+
+}
