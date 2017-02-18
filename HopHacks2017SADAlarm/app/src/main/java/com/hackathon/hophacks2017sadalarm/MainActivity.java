334686a5b377548e56af3e9ac51c10289421ced4
Acheev Bhagat <acheevbhagat@hotmail.com>
2017-02-18 12:50:17 -0500

    Create onTimeSet

--- HopHacks2017SADAlarm/app/src/main/java/com/hackathon/hophacks2017sadalarm/MainActivity.java
+++ HopHacks2017SADAlarm/app/src/main/java/com/hackathon/hophacks2017sadalarm/MainActivity.java
@@ -1,19 +1,26 @@
 package com.hackathon.hophacks2017sadalarm;
 
+import android.app.TimePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TimePicker;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener {
+
+    AlarmReceiver alarm = new AlarmReceiver();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Main process
     }
 
 
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute)
+    {
 
+    }
 
 
 }
