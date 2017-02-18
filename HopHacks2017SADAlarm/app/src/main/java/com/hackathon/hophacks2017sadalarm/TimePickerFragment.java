7a4231ad815bd5e5697c2158e2cc8c96308a2002
Acheev Bhagat <acheevbhagat@hotmail.com>
2017-02-18 14:54:04 -0500

    Just trying random things

--- HopHacks2017SADAlarm/app/src/main/java/com/hackathon/hophacks2017sadalarm/TimePickerFragment.java
+++ HopHacks2017SADAlarm/app/src/main/java/com/hackathon/hophacks2017sadalarm/TimePickerFragment.java
@@ -19,10 +19,10 @@ public class TimePickerFragment extends DialogFragment implements TimePickerDial
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        // Use the current time as the default values for the picker
+        // Use the current alarm as the default values for the picker
         final Calendar c = Calendar.getInstance();
-        int hour = c.get(Calendar.HOUR_OF_DAY);
-        int minute = c.get(Calendar.MINUTE);
+        int hour = c.get(MainActivity.hour);
+        int minute = c.get(MainActivity.minute);
 
         // Create a new instance of TimePickerDialog and return it
         return new TimePickerDialog(getActivity(), this, hour, minute,
@@ -30,6 +30,8 @@ public class TimePickerFragment extends DialogFragment implements TimePickerDial
     }
 
     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        MainActivity.hour = view.getHour();
+        MainActivity.minute = view.getMinute();
         // Do something with the time chosen by the user
         TextView tv1=(TextView) getActivity().findViewById(R.id.alarmTime);
         tv1.setText("Hour: "+view.getHour()+" Minute: "+view.getMinute());
