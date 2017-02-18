2a37f623945b67b1003a5dcd16508a505cd801e1
Acheev Bhagat <acheevbhagat@hotmail.com>
2017-02-17 23:31:51 -0500

    Begin AlarmReceiver

---
+++ HopHacks2017SADAlarm/app/src/main/java/com/hackathon/hophacks2017sadalarm/AlarmReceiver.java
@@ -0,0 +1,25 @@
+package com.hackathon.hophacks2017sadalarm;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.content.WakefulBroadcastReceiver;
+
+import java.util.Calendar;
+
+/**
+ * Created by achee on 2/17/2017.
+ */
+
+public class AlarmReceiver extends WakefulBroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent)
+    {
+
+    }
+
+}
