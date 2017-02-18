8f8747016dd461e45a4149ef643434879684d865
Acheev Bhagat <acheevbhagat@hotmail.com>
2017-02-18 15:13:56 -0500

    Tried placing everything in onCreate

--- HopHacks2017SADAlarm/app/src/main/java/com/hackathon/hophacks2017sadalarm/MainActivity.java
+++ HopHacks2017SADAlarm/app/src/main/java/com/hackathon/hophacks2017sadalarm/MainActivity.java
@@ -15,6 +15,16 @@ public class MainActivity extends AppCompatActivity implements TimePickerDialog.
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TimePicker tp = (TimePicker) findViewById(R.id.timePicker);
+        final TextView alarmTime = (TextView) findViewById(R.id.alarmTime);
+        tp.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+            @Override
+            public void onTimeChanged(TimePicker view, int h, int m) {
+                alarmTime.setText("Hour: " + hour + " Minute: " + minute);
+            }
+        }
+        );
     }
 
 
